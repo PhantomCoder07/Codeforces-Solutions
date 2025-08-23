bb4129a80f89760cbf72534e5d937442bcb730bf
Phantom Coder <abdulahad474783@gmail.com>
2025-08-23 18:51:29 +0600

    Solutions

---
+++ Codeforces/1760/1760 B. Atilla's Favorite Problem.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+        sort(s.begin(),s.end());
+        int c=s[n-1]-'a';
+        cout<<c+1<<endl;
+    }
+    return 0;
+}
