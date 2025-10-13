48f6d2b6f3c4d602d527855281e6f59ccf035ce8
Phantom Coder <abdulahad474783@gmail.com>
2025-10-14 00:06:58 +0600

    Solutions

---
+++ Codeforces/2074/2074 B. The Third Side.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while (t--)
+    {
+        int n,sum=0;
+        scanf("%d",&n);
+        int a[n];
+        for (int i=0; i<n; i++)
+        {
+            scanf("%d",&a[i]);
+            sum+=a[i];
+        }
+        printf("%d\n",sum-n+1);
+    }
+    return 0;
+}
