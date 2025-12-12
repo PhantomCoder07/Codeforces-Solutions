89bb435d81ce42f8be500ab92d34afd20a6e9189
Phantom Coder <abdulahad474783@gmail.com>
2025-12-13 00:43:20 +0600

    Solutions

---
+++ Codeforces/1092/1092 B. Teams Forming.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for (int i=0; i<n; i++)
+    {
+        cin>>a[i];
+    }
+    sort(a.begin(),a.end());
+    int ans=0;
+    for (int i=0; i<n; i+=2)
+    {
+        ans+=abs(a[i+1]-a[i]);
+    }
+    cout<<ans<<endl;
+    return 0;
+}
