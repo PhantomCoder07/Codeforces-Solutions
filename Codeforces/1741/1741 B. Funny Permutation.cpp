15950ac42588b92b654e56a2757e16b128fd7c1c
Phantom Coder <abdulahad474783@gmail.com>
2026-01-05 00:38:34 +0600

    Solutions

---
+++ Codeforces/1741/1741 B. Funny Permutation.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int n;
+        cin>>n;
+        if(n==1||n==3)
+        {
+            cout<<"-1"<<endl;
+        }
+        else
+        {
+            cout<<n<<" "<<n-1<<" ";
+            for(int i=1; i<n-1; i++)
+            {
+                cout<<i<<" ";
+            }
+            cout<<endl;
+        }
+    }
+}
