cd199b10c8b6aadc52281b9841e2fa0ae7fd98e2
ABDUL AHAD <abdulahad474783@gmail.com>
2026-02-27 16:12:34 +0600

    Solutions

---
+++ Codeforces/1684/1684 B. Z mod X = C.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int a,b,c;
+        cin>>a>>b>>c;
+        long long x,y,z;
+        x=a+b+c;
+        y=b+c;
+        z=c;
+        cout<<x<<' '<<y<<' '<<z<<endl;
+    }
+    return 0;
+}
