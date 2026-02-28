d38802f8a7dbcfef2f64aedf47dc2a8191158989
ABDUL AHAD <abdulahad474783@gmail.com>
2026-02-28 20:13:38 +0600

    Solutions

---
+++ Codeforces/1895/1895 B. Points and Minimum Distance.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int n;
+        cin>>n;
+        vector<int> a(2*n);
+        for (int i=0; i<2*n; i++)
+            cin>>a[i];
+        sort(a.begin(),a.end());
+        int sum=0;
+        for (int i=0; i<n-1; i++)
+            sum+=abs(a[i]-a[i+1]);
+        for (int i=n; i<2*n-1; i++)
+            sum+=abs(a[i]-a[i+1]);
+        cout<<sum<<endl;
+        for (int i=0; i<n; i++)
+        {
+            cout<<a[i]<<' '<<a[2*n-1-i]<<endl;
+        }
+    }
+    return 0;
+}
