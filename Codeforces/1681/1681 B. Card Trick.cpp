71aa5ce038db4f3c3a17dd86f7dd1143ddfda6c8
ABDUL AHAD <abdulahad474783@gmail.com>
2026-03-03 23:23:42 +0600

    Solutions

---
+++ Codeforces/1681/1681 B. Card Trick.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int n,m,sum=0;
+        cin>>n;
+        vector<int> a(n);
+        for (int i=0; i<n; i++)
+            cin>>a[i];
+        cin>>m;
+        vector<int> b(m);
+        for (int i=0; i<m; i++)
+        {
+            cin>>b[i];
+            sum=(sum+b[i])%n;
+        }
+        cout<<a[sum]<<endl;
+    }
+    return 0;
+}
