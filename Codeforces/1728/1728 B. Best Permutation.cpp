71f252c7966abfbdef62b6452e79ccf63de1301e
ABDUL AHAD <abdulahad474783@gmail.com>
2026-03-03 23:24:30 +0600

    Solutions

---
+++ Codeforces/1728/1728 B. Best Permutation.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int n;
+        cin>>n;
+        if (n%2==0)
+        {
+            for (int i=n-2; i>0; i--)
+                cout<<i<<' ';
+            cout<<n-1<<' '<<n<<endl;
+        }
+        else
+        {
+            cout<<1<<' ';
+            for (int i=n-2; i>1; i--)
+                cout<<i<<' ';
+            cout<<n-1<<' '<<n<<endl;
+        }
+    }
+    return 0;
+}
