3fd754206c6c12ffbd80f2a89b2679ba9a782719
ABDUL AHAD <abdulahad474783@gmail.com>
2026-03-07 21:56:23 +0600

    Solutions

---
+++ Codeforces/1686/1686 B. Odd Subarrays.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int n,c=0;
+        cin>>n;
+        vector<int> a(n);
+        for (int i=0; i<n; i++)
+            cin>>a[i];
+        for (int i=0; i<n-1; i++)
+        {
+            if (a[i]>a[i+1])
+            {
+                c++;
+                i++;
+            }
+        }
+        cout<<c<<endl;
+    }
+    return 0;
+}
