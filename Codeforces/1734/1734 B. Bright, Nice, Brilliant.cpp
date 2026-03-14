f9ad53e7292975e1956c1f7ffdd533fa2f3a7472
ABDUL AHAD <abdulahad474783@gmail.com>
2026-03-14 13:17:56 +0600

    Solutions

---
+++ Codeforces/1734/1734 B. Bright, Nice, Brilliant.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int n;
+        cin>>n;
+        for (int i=0; i<n; i++)
+        {
+            for (int j=0; j<=i; j++)
+            {
+                if (j==0 || i==j)
+                    cout<<1<<' ';
+                else
+                    cout<<0<<' ';
+            }
+            cout<<endl;
+        }
+    }
+    return 0;
+}
