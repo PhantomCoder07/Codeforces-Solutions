a89510bd24971eb48fb4f7687b3a36efb52f1eea
ABDUL AHAD <abdulahad474783@gmail.com>
2026-03-29 16:54:06 +0600

    Solutions

---
+++ Codeforces/1828/1828 B. Permutation Swap.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int n,ans=0;
+        cin>>n;
+        vector<int> a(n);
+        for (int i=0; i<n; i++)
+        {
+            cin>>a[i];
+            ans=__gcd(ans,abs(a[i]-(i+1)));
+        }
+        cout<<ans<<endl;
+    }
+    return 0;
+}
