7cd6fe6a1e3f6f9201090f1f3050340e476035e3
ABDUL AHAD <abdulahad474783@gmail.com>
2026-04-17 21:55:36 +0600

    Solutions

---
+++ Codeforces/2013/2013 B. Battle for Survive.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int n;
+        cin>>n;
+        vector<int> a(n);
+        for (int i=0; i<n; i++)
+            cin>>a[i];
+        cout<<accumulate(a.begin(),a.end(),0LL)-2*a[n-2]<<endl;
+    }
+    return 0;
+}
