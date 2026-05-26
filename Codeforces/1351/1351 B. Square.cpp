f1b3e2bddbfedbc3390a37a04f9fb3b9b2b3c614
ABDUL AHAD <abdulahad474783@gmail.com>
2026-05-26 22:02:00 +0600

    Solutions

---
+++ Codeforces/1351/1351 B. Square.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int a1,a2,b1,b2;
+        cin>>a1>>b1>>a2>>b2;
+        if (a1<b1)
+            swap(a1,b1);
+        if (a2<b2)
+            swap(a2,b2);
+        if (a1==a2 && b1+b2==a1)
+            cout<<"YES"<<endl;
+        else
+            cout<<"NO"<<endl;
+    }
+    return 0;
+}
