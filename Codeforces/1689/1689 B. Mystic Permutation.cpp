f59734385a1f8b2bd99da33463e4cebde7e5ff83
ABDUL AHAD <abdulahad474783@gmail.com>
2026-05-26 22:05:17 +0600

    Solutions

---
+++ Codeforces/1689/1689 B. Mystic Permutation.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int n;
+        cin>>n;
+        vector<int> a(n),b(n);
+        for (int i=0; i<n; i++)
+        {
+            cin>>a[i];
+            b[i]=i+1;
+        }
+        if (n==1)
+        {
+            cout<<-1<<endl;
+            continue;
+        }
+        for (int i=0; i<n-1; i++)
+        {
+            if (a[i]==b[i])
+                swap(b[i],b[i+1]);
+        }
+        if (a[n-1]==b[n-1])
+            swap(b[n-2],b[n-1]);
+        for (int i=0; i<n; i++)
+            cout<<b[i]<<' ';
+        cout<<endl;
+    }
+    return 0;
+}
