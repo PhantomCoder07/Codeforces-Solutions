ea27d3fe6da5627e6e39890f4a1c8045e7553948
ABDUL AHAD <abdulahad474783@gmail.com>
2026-06-20 14:21:06 +0600

    Solutions

---
+++ Codeforces/1612/1612 B. Special Permutation.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int n,a,b;
+        cin>>n>>a>>b;
+        if ((a<=n/2 && b>n/2) || (a==n/2+1 && b==n/2))
+        {
+            cout<<a<<' ';
+            for (int i=n; i>0; i--)
+            {
+                if (i!=a && i!=b)
+                    cout<<i<<' ';
+            }
+            cout<<b<<endl;
+        }
+        else
+            cout<<-1<<endl;
+    }
+    return 0;
+}
