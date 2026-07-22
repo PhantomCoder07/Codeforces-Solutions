ac5426e0481983c6fa79c578d2496bb8dd05a87e
ABDUL AHAD <abdulahad474783@gmail.com>
2026-07-22 18:33:14 +0600

    Solutions

---
+++ Codeforces/1093/1093 B. Letters Rearranging.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        string s;
+        cin>>s;
+        int n=s.length();
+        sort(s.begin(),s.end());
+        if (s[0]==s[n-1])
+            cout<<-1<<endl;
+        else
+            cout<<s<<endl;
+    }
+    return 0;
+}
